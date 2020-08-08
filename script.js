d3bc4c1cf14213dc3cd5c13d29319a9d2627c312
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-08-08 10:26:43 +0300

    worked on the modal

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+console.log('it works');
 const foods = [{
         id: 'ravitoto',
         price: 5000,
@@ -34,3 +35,31 @@ const foods = [{
         vegetarian: false,
     },
 ];
+
+// create the modal to show the total cost 
+// grab modal
+const innerModal = document.querySelector('.inner-modal');
+const outerModal = document.querySelector('.outer-modal');
+
+// open modal
+const showModal = () => {
+        // add class open
+        outerModal.classList.add('open');
+        // showing the final total
+
+    }
+    // listen for the confirm btn
+window.addEventListener('click', event => {
+    if (event.target.matches('.confirm-btn')) {
+        showModal();
+    }
+});
+
+// handle escape key
+const handleEsc = event => {
+    if (event.key === "Escape") {
+        outerModal.classList.remove('open');
+    }
+};
+
+window.addEventListener('keyup', handleEsc);
